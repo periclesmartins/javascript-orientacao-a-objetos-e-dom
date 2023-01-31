b6811aaea5c23908120b11f03821af88dd86f7dc
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-31 01:24:28 -0300

    Aula de variáveis.

---
+++ variaveis/aula1.js
@@ -0,0 +1,13 @@
+console.log('Hello World') // Apresenta a mensagem Hello World na tela.
+
+const nomeCompleto = 'Péricles Peres Maciel Martins'
+let idade = 25
+let dataNascimento = '29.03.1997'
+let sexo = 'M'
+let peso = 66.5
+let deficiencia = false //false = falso, true = verdadeiro (lógica booleana)
+let listaNumerica = [1,2,3,4,5,6,7,8,9] //Armazenando mais de um dado na variável.
+console.log(`O nome completo é: ${nomeCompleto}.`)
+nomeCompleto = 'Pericles Peres' 
+console.log(`O nome e sobrenome é: ${nomeCompleto}.`)
+// Mudei o valor dentro da variável nomeCompleto, mas ao mudar de let, para const, não é possível mudar, pois const = constante, ou seja, ao declarar uma constante, ela não pode ser mudada.
