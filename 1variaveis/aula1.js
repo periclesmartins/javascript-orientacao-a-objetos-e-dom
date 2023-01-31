7cd675bfdba64ff9c8f677ce6e3d6728fdd02007
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-31 19:42:40 -0300

    Comentei o código!
    Coloquei um comentário importante na aula de variáveis.

--- 1variaveis/aula1.js
+++ 1variaveis/aula1.js
@@ -12,3 +12,13 @@ console.log(`O nome completo é: ${nomeCompleto}.`)
 nomeCompleto = 'Pericles Peres' 
 console.log(`O nome e sobrenome é: ${nomeCompleto}.`)
 // Mudei o valor dentro da variável nomeCompleto, mas ao mudar de let, para const, não é possível mudar, pois const = constante, ou seja, ao declarar uma constante, ela não pode ser mudada.
+
+/*
+Em JavaScript, existem três tipos de variáveis:
+
+Variáveis Primitivas: São valores básicos que não podem ser divididos em outros valores. Os tipos primitivos incluem Number, String, Boolean, Undefined e Null.
+
+Variáveis de Referência: São objetos que armazenam múltiplos valores. Incluem Array, Object, Function, etc.
+
+Variáveis Globais e Locais: Uma variável global é acessível em todo o escopo da aplicação, enquanto uma variável local é acessível apenas dentro do escopo da função onde ela é declarada.
+*/
