f47714ddd40dc71d7f8e7eeb826747af3d34eb95
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-31 21:06:26 -0300

    Create aula2.js

---
+++ estrutura-condicional/aula2.js
@@ -0,0 +1,14 @@
+// Estrutura condicional
+let login = 'pamella'
+let senha = '123'
+
+// e (and) &&
+//ou (or) ||
+if (login === 'pamella') {
+    console.log('Pâmella, seu login está correto.')
+} if (senha == 123) {
+    console.log('Sua senha está correta.')
+}
+console.log('Seja bem-vinda!')
+
+//Se for colocado if abaixo de if, todos os ifs serão executados.
