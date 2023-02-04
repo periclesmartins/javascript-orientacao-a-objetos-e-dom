c733f2e351aa36c0e5dd2d1373a827ac87ea0811
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-04 03:48:39 -0300

    aula de programação assíncrona
    JS Async

---
+++ 9promises/index.js
@@ -0,0 +1,15 @@
+let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+  // The producing code (this may take some time)
+  
+    if (x == 0) {
+      myResolve("OK");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then((erro) => {
+    console.log(erro)
+  })
